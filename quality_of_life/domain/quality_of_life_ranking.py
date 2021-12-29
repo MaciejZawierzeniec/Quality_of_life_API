16af2d4de8775b8739d1f6d0884b1dd91d921dd8
Maciej Zawierzeniec <mzawierzeniec@gmail.com>
2021-12-29 14:09:28 +0100

    Find the best cities

---
+++ quality_of_life/domain/quality_of_life_ranking.py
@@ -0,0 +1,10 @@
+from typing import Dict, List
+
+from repo.repository import read_dataset
+
+
+def get_ranking() -> List[Dict]:
+    qol = read_dataset('/home/maciej/PycharmProjects/QualityOfLifeAPI/quality_of_life_extended.csv')
+    ranking = [{'city': element[0], 'ranking': element[1]} for element in list(zip(qol['UA_Name'], qol['mean']))]
+    return sorted(ranking, key=lambda x: x['ranking'], reverse=True)
+
