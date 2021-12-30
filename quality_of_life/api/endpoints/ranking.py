e43d30ffb835bb9541b42efa03b48b273c1f21f8
Maciej Zawierzeniec <mzawierzeniec@gmail.com>
2021-12-30 14:09:25 +0100

    Add tests for the distance_calculator

---
+++ quality_of_life/api/endpoints/ranking.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+from domain.quality_of_life_ranking import get_city_ranking
+
+
+router = APIRouter()
+
+
+@router.get("/ranking/")
+async def get_ranking(limit: int = 1):
+    return get_city_ranking(limit)
