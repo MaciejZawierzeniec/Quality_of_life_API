62251d8869ab87148edfe43a20e2a5f9589db02c
Maciej Zawierzeniec <mzawierzeniec@gmail.com>
2022-01-03 10:18:07 +0100

    Add setup.py

---
+++ quality_of_life/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='QualityOfLifeAPI',
+    version='0.1.0',
+    packages=['api', 'api.endpoints', 'repo', 'domain'],
+    package_dir={'': '../quality_of_life'},
+    url='https://github.com/MaciejZawierzeniec/Quality_of_life_API',
+    license='',
+    author='Maciej',
+    author_email='mzawierzeniec@gmail.com',
+    description='API for the quality_of_life dataset results',
+    install_requires=['pytest~=6.2.5',
+                      'pytest-cov~=3.0.0',
+                      'fastapi~=0.70.1',
+                      'pandas~=1.3.5',
+                      'geopy~=2.2.0',
+                      'OSMPythonTools~=0.3.3',
+                      'requests~=2.26.0',
+                      ],
+)
