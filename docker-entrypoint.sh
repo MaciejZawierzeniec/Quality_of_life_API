9250e3a3c0544216b992b1777cce98e19a683dfc
Maciej Zawierzeniec <maciej.zawierzeniec@stxnext.pl>
2022-01-19 17:14:34 +0100

    squash

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,6 @@ set -e
 
 
 python quality_of_life/db/populate_db.py &
-python -m quality_of_life
+python -m quality_of_life &
+pytest --cov=quality_of_life --cov-fail-under=65 tests/unit/api/endpoints/test_ranking.py
 # tail -f /dev/null
